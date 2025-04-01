f97fa2d53c9512aead17893dc55bbe309ea837c3
itsrofly <rocketeman1234@gmail.com>
2025-04-01 14:02:42 +0100

    Projects Header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -99,26 +99,24 @@ export default component$(() => {
 
         <div class="d-flex flex-row justify-content-around mt-5">
           {/* Scroll Down */}
-          <div class="d-inline-flex gap-3" style={{ cursor: "pointer" }} onClick$={(e) => {
-            window.scrollBy(0, 650);
-          }}>
+          <a class="d-inline-flex gap-3 link-underline link-underline-opacity-0" href="/#Projects">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-arrow-down-circle terciary-color" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z" />
             </svg>
-            <span class="primary-color">Scroll</span>
-          </div>
+            <span class="primary-color" id="Projects">Scroll</span>
+          </a>
           <div style={{ color: "#242424" }}>©ItsRofly. All Rights Reserved.</div>
         </div>
       </div>
 
       {/* Projects */}
-      <div class="vh-100 v-100 text-white">
+      <div class="v-100 min-vh-100">
         <div class="m-auto h-100 w-75">
           <div class="mb-4">
             <span class="primary-color">Projects</span>
           </div>
           {/* Filter */}
-          <div class="ps-3 d-inline-flex gap-3">
+          <div class="ps-3 d-inline-flex gap-3 mb-1">
             <a class={"btn btn-outline-light" + (selecTag.value == 0 ? " active" : "")} onClick$={() => selecTag.value = 0}
              role="button" style={{ "--bs-btn-padding-x": "2rem", "--bs-btn-padding-y": "0.1rem" }}>All</a>
 
@@ -132,6 +130,9 @@ export default component$(() => {
 
             <a class={"btn btn-outline-light" + (selecTag.value == 3 ? " active" : "")} onClick$={() => selecTag.value = 3} 
             role="button" style={{ "--bs-btn-padding-x": "2rem", "--bs-btn-padding-y": "0.1rem" }}>Frontend</a>
+          </div>
+          <div class="ps-4">
+          <span class="secondary-color" style={{ fontSize: "0.8rem" }}>Select tag</span>
           </div>
         </div>
       </div>
