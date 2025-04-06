efc5836b35f88db0804d0c6bfde9cb0fd28c2400
itsrofly <rocketeman1234@gmail.com>
2025-04-06 14:28:07 +0100

    fix build

--- src/routes/blog/[id]/index.tsx
+++ src/routes/blog/[id]/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import { type DocumentHead, useLocation, routeLoader$ } from "@builder.io/qwik-city";
+import { type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
 
 export const usePost = routeLoader$(async (requestEvent) => {
   // Fetch post info
